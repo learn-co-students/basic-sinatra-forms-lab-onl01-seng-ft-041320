230447765a138c91af1c27eee26d76934c00129d
Jaeson Watts <jaeson.watts@gmail.com>
2020-06-01 03:02:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,19 +1,15 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
+  
   get '/newteam' do
-    @team = Team.new(params["name"], params["coach"],params["pg"],params["sg"],params["sf"],params["pf"],params["c"])
     erb :newteam
   end
 
-  post '/team' do
-    
+  post '/newteam' do
+    @team = params
     erb :team
   end
-  
-  
-  
   
   
   
